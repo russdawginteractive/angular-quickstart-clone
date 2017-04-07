998dc3467a713810bee23e772e01a62d4e0e8feb
Russell T. Griffith <russell.griffith@gmail.com>
2017-04-07 16:55:08 -0400

    Completed example and commited all unstaged files: https://angular.io/docs/ts/latest/guide/attribute-directives.html

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive ({ 
+    selector: '[myHighlight]'
+})
+
+export class HighlightDirective {
+    @Input('myHighlight') highlightColor: string;
+    @Input() defaultColor: string;
+    constructor(private el: ElementRef) {}
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight(this.highlightColor || this.defaultColor || 'red');
+    }
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight(null);
+    }
+
+}
